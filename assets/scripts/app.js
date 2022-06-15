a2e4008323c0aa8db172afb01bf744cabcf09052
elnur004 <lnur004@gmail.com>
2022-06-16 02:25:10 +0400

    added more 'if' statements and a 'strongAttack' functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
-const ATTACK_VALUE = 10;  // Global hardcoded GAME_VALUE convention (optional)
+const ATTACK_VALUE = 10; // Global hardcoded GAME_VALUE convention (optional)
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterLife = chosenMaxLife;
@@ -6,20 +8,36 @@ let currentPlayerLife = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+
+  const monsterDamage = dealMonsterDamage(maxDamage);
+  currentMonsterLife -= monsterDamage;
+
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerLife -= playerDamage;
+
+  if (currentMonsterLife <= 0 && currentPlayerLife > 0) {
+    console.log('You won!');
+  } else if (currentMonsterLife > 0 && currentPlayerLife <= 0) {
+    console.log('Monster won!');
+  } else if (currentMonsterLife <= 0 && currentPlayerLife <= 0) {
+    console.log('You have a draw!');
+  }
+}
+
 function attackHandler() {
-    const monsterDamage = dealMonsterDamage(ATTACK_VALUE);
-    currentMonsterLife -= monsterDamage;
+  attackMonster('ATTACK');
+}
 
-    const playerDamage = dealPlayerDamage(ATTACK_VALUE);
-    currentPlayerLife -= playerDamage;
-
-    if(currentMonsterLife <= 0 && currentPlayerLife > 0) {
-        console.log('You won!');
-    } else if(currentMonsterLife > 0 && currentPlayerLife <= 0) {
-        console.log('Monster won!');
-    } else if(currentMonsterLife <= 0 && currentPlayerLife <= 0) {
-        console.log('You have a draw!');
-    }
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
